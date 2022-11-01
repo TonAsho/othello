e24ccf78292dda34857cb5c7603245d4c48aa1a6
Ramurata <87478591+TonAsho@users.noreply.github.com>
2022-11-02 07:59:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ function yech(id) {
         else turn = 1;
     }
     else console.log("NoNoNo! error!"); 
-    can_turn = [], now = [];
 }
 function change(h, w) {
+    can_turn = [], now = [];
     for(let i = 0; i < 8; ++i) {
         hh = h, ww = w;
         cnt = 0;
@@ -71,5 +71,11 @@ function start() {
         let add = document.createElement("div");
         if(field[i][j] == 1) add.className = "black", document.getElementById(String(8 * i + (j + 1))).appendChild(add);
         if(field[i][j] == -1) add.className = "white", document.getElementById(String(8 * i + (j + 1))).appendChild(add);
+    }
+}
+function place() {
+    for(let i = 0; i < H; ++i) for(let j = 0; j < W; ++j) {
+        if(field[i][j] != 0) continue;
+        if(change[i][j]) document.getElementById(String(8 * i + (j + 1))).style.backgroundColor = "red";
     }
 }
